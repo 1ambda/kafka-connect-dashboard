ad426a762eabb45afb6ee009e184e47bbf803499
1ambda <1amb4a@gmail.com>
2016-05-07 23:07:49 +0900

    fix: Enable CORS in storage

--- kafkalot-storage/src/main/scala/io/github/lambda/Application.scala
+++ kafkalot-storage/src/main/scala/io/github/lambda/Application.scala
@@ -3,8 +3,11 @@ package io.github.lambda
 import com.twitter.finagle.Http
 import com.twitter.util.Await
 import io.github.lambda.api.ConnectorApi
+import io.github.lambda.security.CorsFilter
 
 object Application extends App {
 
-  Await.ready(Http.server.serve(":8081", ConnectorApi.api))
+  val corsFilter = new CorsFilter
+
+  Await.ready(Http.server.serve(":3003", corsFilter andThen ConnectorApi.api))
 }
---
+++ kafkalot-storage/src/main/scala/io/github/lambda/security/CorsFilter.scala
@@ -0,0 +1,20 @@
+package io.github.lambda.security
+
+import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.finagle.http.filter.Cors
+import com.twitter.finagle.http.filter.Cors.HttpFilter
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.util.Future
+
+class CorsFilter extends SimpleFilter[Request, Response] {
+  val allowsOrigin  = { origin: String => Some(origin) }
+  val allowsMethods = { method: String => Some(Seq("GET", "POST", "PUT", "DELETE")) }
+  val allowsHeaders = { headers: Seq[String] => Some(headers) }
+
+  val policy = Cors.Policy(allowsOrigin, allowsMethods, allowsHeaders, supportsCredentials = true)
+  val cors = new HttpFilter(Cors.UnsafePermissivePolicy)
+
+  override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
+    cors.apply(request, service)
+  }
+}
